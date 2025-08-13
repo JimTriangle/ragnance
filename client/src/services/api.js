31993f93f4a257d9e9043a852037f50a1bdc6ac7
JimTriangle <jmariepujade@gmail.com>
2025-08-13 21:04:22 +0200

    correction route api en dev

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const baseURL = (process.env.REACT_APP_API_URL?.replace(/\/$/, "")) || "/api";
 
+const productionUrl = 'https://ragnance.fr/api'; 
+
+const baseURL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:5000/api'
+  : productionUrl;
 const api = axios.create({
   baseURL,
   headers: { "Content-Type": "application/json" },
