9fd3225cdad5e48cea8c0f7173548c43a0e3c4f9
JimTriangle <jmariepujade@gmail.com>
2025-08-13 19:20:10 +0200

    modif pour l'acces aux donnees

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,27 @@
 import axios from "axios";
-const baseURL = (process.env.REACT_APP_API_URL?.replace(/\/$/,"")) || "/api";
+
+const baseURL = (process.env.REACT_APP_API_URL?.replace(/\/$/, "")) || "/api";
+
 const api = axios.create({
   baseURL,
   headers: { "Content-Type": "application/json" },
-  withCredentials: true,            // <--- ajoute ceci si tu utilises des cookies
+  withCredentials: true,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    // On récupère le token depuis le localStorage
+    const token = localStorage.getItem('authToken');
+
+    // Si le token existe, on l'ajoute dans les en-têtes (headers)
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
