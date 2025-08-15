1c60c592d3254078d817645d22133753bd901264
JimTriangle <jmariepujade@gmail.com>
2025-08-15 16:28:52 +0200

    correction theme jour/nuit

--- client/src/context/ThemeContext.js
+++ client/src/context/ThemeContext.js
@@ -1,25 +1,23 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useRef } from 'react';
 import PrimeReact from 'primereact/api';
-import 'primereact/resources/themes/saga-blue/theme.css';
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(() => localStorage.getItem('theme') || 'saga-blue');
+  const previousTheme = useRef('saga-blue');
 
   useEffect(() => {
-    if (theme !== 'saga-blue') {
-      PrimeReact.changeTheme('saga-blue', theme, 'theme-link');
-    }
-  }, []);
+    PrimeReact.changeTheme(previousTheme.current, theme, 'theme-link');
+    previousTheme.current = theme;
+  }, [theme]);
 
   useEffect(() => {
     localStorage.setItem('theme', theme);
   }, [theme]);
 
   const toggleTheme = () => {
-    const newTheme = theme === 'saga-blue' ? 'vela-blue' : 'saga-blue';
-    PrimeReact.changeTheme(theme, newTheme, 'theme-link', () => setTheme(newTheme));
+     setTheme((prevTheme) => (prevTheme === 'saga-blue' ? 'vela-blue' : 'saga-blue'));
   };
 
   return (
