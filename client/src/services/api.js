2b0b68e5d59984aad4c939d5343a213412b283db
JimTriangle <jmariepujade@gmail.com>
2025-09-20 18:22:32 +0200

    fix login auth error

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -28,4 +28,17 @@ api.interceptors.request.use(
   }
 );
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error?.response?.status === 401) {
+      localStorage.removeItem('authToken');
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
